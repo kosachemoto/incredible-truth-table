37f4a1ded341f5f3e1267b7305c4aa2cdab8fdda
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2018-11-03 08:01:53 +0300

    Convertet to a React project

---
+++ src/containers/main/Main.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./Main.css";
+
+class Main extends Component {
+  render() {
+    return (
+      <>
+        <h1 className="main-header">Incredible Truth Table</h1>
+        <p>Now via React.js...</p>
+      </>
+    );
+  }
+}
+
+export default Main;

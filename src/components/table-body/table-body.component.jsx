e48915fdaf6e1e203303a807355e809c99b49295
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2018-11-24 04:06:02 +0300

    Project restructurization

---
+++ src/components/table-body/table-body.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class TableBodyComponent extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <>
+        This is TableBodyComponent Component!
+      </>
+    )
+  }
+}

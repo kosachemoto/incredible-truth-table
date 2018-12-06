78339478e2c30951b6f81e7ac882e20d5c5c0c9f
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2018-12-06 03:31:21 +0300

    Add new statick things

--- src/services/expression/expression.service.js
+++ src/services/expression/expression.service.js
@@ -1,4 +1,5 @@
 export class ExpressionService {
+  // Parse expression and retun all variables
   static parse(expression) {
     const regexp = /[\s!&|()=<>]|\btrue\b|\bfalse\b|\s/gi;
     const variables = expression.split(regexp);
@@ -7,5 +8,18 @@ export class ExpressionService {
         return element !== "" && self.indexOf(element) === index; 
       });
   }
+  // Return set of values from `position` position 
+  // and for truth table with `varCourn` variables
+  static sourceDataValues(varCount, position) {
+    return position
+      .toString(2)
+      .padStart(+varCount, "0")
+      .split("")
+      .map(value => Boolean(+value))
   }
+  // Return executable function by `expression`
+  // with appropriate count of variables-parameters
+  static execFunctionBy(expression) {
+    return 0;
+  } 
 }
